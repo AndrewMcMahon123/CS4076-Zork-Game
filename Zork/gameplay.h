58a1751ab82eb545f118be843e5c7cf0b3e00b0b
AndrewMcMahon123 <53841605+AndrewMcMahon123@users.noreply.github.com>
2021-03-15 14:12:25 +0000

    Update gameplay.h

--- Zork/gameplay.h
+++ Zork/gameplay.h
@@ -1,11 +1,21 @@
 #ifndef GAMEPLAY_H
 #define GAMEPLAY_H
 
+#include <QString>
+#include "room.h"
 
 class gameplay
 {
+
+private:
+    Room *currentRoom;
+    void createRooms();
+    void addItemsToRoom(item);
+
 public:
     gameplay();
+    Room* getCurrentRoom();
+    QString move(int direction);
 };
 
 #endif // GAMEPLAY_H
