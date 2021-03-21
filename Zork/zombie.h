0f411e843602a48194ead09c2babc35a48f1cd08
woodsy104 <55923959+woodsy104@users.noreply.github.com>
2021-03-21 15:43:10 +0000

    Update zombie.h

--- Zork/zombie.h
+++ Zork/zombie.h
@@ -2,15 +2,24 @@
 #define ZOMBIE_H
 #include <iostream>
 #include <string>
+#include <QFileDialog>
 using namespace std;
 
 class zombie
 {
 private:
     string type;
-
+    QString image;
+    string question;
+    int difficulty;
 public:
-    zombie(string type);
+   zombie(string type, QString image, string question, int difficulty); //png for creation add image
+   string getType();
+   void setQuestion(string question);
+   string getQuestion();
+   int getDifficulty();
+   string randomQ(int difficulty);
 };
 
 #endif // ZOMBIE_H
+
